bef0b4f6ad4e152c5a7d49e8030b651181ed986a
wangxiang <wangxiangc@gmail.com>
2013-12-30 09:16:22 +0800

    update bat pause

--- bat/docbook.bat
+++ bat/docbook.bat
@@ -1,2 +1,4 @@
 cd %~dp0
 xsltproc -o ../bookdoc/docbook.html ../g.xsl ../bookdoc/docbook.xml
+pause
+exit
--- bat/git.bat
+++ bat/git.bat
@@ -1,2 +1,4 @@
 cd %~dp0
 xsltproc -o ../git/git.html ../g.xsl ../git/git.xml
+pause
+exit
